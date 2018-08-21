28ac2794dfc2338ac54781fc35a82abd5eb7dde7
Christian Romney <cromney@pointslope.com>
2018-08-20 22:03:24 -0400

    Use Clojure 1.9.0
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/christianromney/quiz-compile"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/spec.alpha "0.2.168"]]
   :main ^:skip-aot quiz-compile.core
   :target-path "target/%s"
